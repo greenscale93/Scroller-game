be0f35f1041387c3bbbe9d3a601edbdb920f9d7f
greenscale93 <greenscale93@gmail.com>
2018-11-21 13:17:15 +0300

    New background and player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 15f;
+
+    //cashe
+    private float cameraSize;
+
+	// Use this for initialization
+	void Start () {
+        cameraSize = Camera.main.orthographicSize;
+        Debug.Log(cameraSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();	
+	}
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal")*Time.deltaTime*moveSpeed;
+        float newPosX = Mathf.Clamp(transform.position.x + deltaX,-4,4);
+
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        float newPosY = transform.position.y + deltaY;
+
+
+        transform.position = new Vector2(newPosX, newPosY);
+    }
+}
